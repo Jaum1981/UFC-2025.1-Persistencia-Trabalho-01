792dd4bf459c88194e4a34b7f18cf13a4e6bf1fb
Francisco Breno <fbreno.dev@gmail.com>
2025-05-13 12:17:20 -0300

    feat: Rota de filtragem de tickets e refactor das configurações de arquivo e log.

--- routers/session.py
+++ routers/session.py
@@ -1,19 +1,19 @@
 import os
-import json
 import zipfile
-import yaml
-import logging.config
 from datetime import datetime
 from fastapi import APIRouter, HTTPException, Query
 from starlette.responses import FileResponse
 from http import HTTPStatus
 from models.models import Session
-from typing import List
+from typing import List, Optional
 from utils.logger_config import logger
 
 router = APIRouter()
-SESSION_CSV_FILE = 'data/session.csv'
-SESSION_ZIP_FILE = 'compressed/session.zip'
+from utils.configs import ler_config_yaml
+
+sessions_data = ler_config_yaml().get('data', {})
+SESSION_CSV_FILE = sessions_data.get('csv', {}).get('session', 'data/session.csv')
+SESSION_ZIP_FILE = sessions_data.get('compressed', {}).get('session', 'compressed/session.zip')
 
 # Utility functions
 
@@ -47,65 +47,66 @@ def write_session_csv(sessions):
 
 @router.get("/sessions", response_model=List[Session])
 def get_sessions():
-    logger.info("Fetching all sessions")
+    logger.info("[get_sessions] - Fetching all sessions")
     sessions = read_session_csv()
+    logger.info("[get_sessions] - Sessions recovered successfully.")
     return sessions
 
 @router.get("/sessions/{session_id}", response_model=Session)
 def get_session_by_id(session_id: int):
-    logger.info(f"Fetching session with ID: {session_id}")
+    logger.info(f"[get_session_by_id] - Fetching session with ID: {session_id}")
     sessions = read_session_csv()
     for session in sessions:
         if session.id == session_id:
-            logger.debug(f"Session found: {session}")
+            logger.info(f"[get_session_by_id] - Session found: {session}")
             return session
-    logger.error(f"Session with ID {session_id} not found")
+    logger.error(f"[get_session_by_id] - Session with ID {session_id} not found")
     raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Session not found")
 
 @router.post("/sessions", response_model=Session, status_code=HTTPStatus.CREATED)
 def create_session(session: Session):
-    logger.info(f"Creating session: {session}")
+    logger.info(f"[create_session] - Creating session: {session}")
     sessions = read_session_csv()
     if any(s.id == session.id for s in sessions):
-        logger.error(f"Session with ID {session.id} already exists")
+        logger.error(f"[create_session] - Session with ID {session.id} already exists")
         raise HTTPException(status_code=HTTPStatus.CONFLICT, detail="Session with this ID already exists")
     sessions.append(session)
     write_session_csv(sessions)
-    logger.debug(f"Session created: {session}")
+    logger.info(f"[create_session] - Session created: {session}")
     return session
 
 @router.put("/sessions/{session_id}", response_model=Session)
 def update_session(session_id: int, updated_session: Session):
-    logger.info(f"Updating session with ID: {session_id}")
+    logger.info(f"[update_session] - Updating session with ID: {session_id}")
     sessions = read_session_csv()
     for index, session in enumerate(sessions):
         if session.id == session_id:
             sessions[index] = updated_session
             if updated_session.id != session_id:
-                logger.error(f"Cannot change session ID from {session_id} to {updated_session.id}")
+                logger.error(f"[update_session] - Cannot change session ID from {session_id} to {updated_session.id}")
                 raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail="Cannot change session ID")
             write_session_csv(sessions)
-            logger.debug(f"Session updated: {updated_session}")
+            logger.info(f"[update_session] - Session updated: {updated_session}")
             return updated_session
-    logger.error(f"Session with ID {session_id} not found")
+    logger.error(f"[update_session] - Session with ID {session_id} not found")
     raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Session not found")
 
 @router.delete("/sessions/{session_id}", status_code=HTTPStatus.NO_CONTENT)
 def delete_session(session_id: int):
-    logger.info(f"Deleting session with ID: {session_id}")
+    logger.info(f"[delete_session] - Deleting session with ID: {session_id}")
     sessions = read_session_csv()
     for session in sessions:
         if session.id == session_id:
             sessions.remove(session)
             write_session_csv(sessions)
-            logger.debug(f"Session deleted: {session}")
+            logger.info(f"[delete_session] - Session deleted: {session}")
             return
-    logger.error(f"Session with ID {session_id} not found")
+    logger.error(f"[delete_session] - Session with ID {session_id} not found")
     raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Session not found")
 
 @router.get("/sessions-count")
 def get_sessions_count():
-    logger.info("Counting all sessions")
+    logger.info("[get_sessions_count] - Counting all sessions")
     sessions = read_session_csv()
     return  {
         "quantidade": len(sessions)
@@ -113,59 +114,46 @@ def get_sessions_count():
 
 @router.get("/sessions-zip")
 def get_sessions_zip():
-    logger.info("Creating ZIP file of all sessions")
+    logger.info("[get_sessions_zip] - Creating ZIP file of all sessions")
     with zipfile.ZipFile(SESSION_ZIP_FILE, 'w') as zipf:
         zipf.write(SESSION_CSV_FILE, os.path.basename(SESSION_CSV_FILE))
         return FileResponse(SESSION_ZIP_FILE, media_type="application/zip", filename=os.path.basename(SESSION_ZIP_FILE))
 
-@router.get("/sessions-by-attributes", response_model=List[Session])
-def get_sessions_by_attribute(
-    field: str = Query(..., description="Coluna para busca (e.g. id, movie_id, start_time, room, available_seats)"),
-    value: str = Query(..., description="Valor a ser buscado na coluna")
+@router.get("/sessions-filter", response_model=List[Session])
+def filter_sessions(
+    movie_id: Optional[int] = Query(None, description="ID da sessão"),
+    room: Optional[str] = Query(None, description="Sala da sessão"),
+    start_time_from: Optional[datetime] = Query(None, description="Data/hora de início mínima (ISO format)"),
+    start_time_to: Optional[datetime] = Query(None, description="Data/hora de início máxima (ISO format)"),
+    available_seat: Optional[str] = Query(None, description="Assento disponível")
 ):
+    logger.info("[filter_sessions] - Starting search with sessions filtering.")
+    logger.debug("[filter_sessions] - Filtering attributes:")
+    logger.debug(f"[filter_sessions] - movie_id: {movie_id}")
+    logger.debug(f"[filter_sessions] - room: {room}")
+    logger.debug(f"[filter_sessions] - start_time_from: {start_time_from}")
+    logger.debug(f"[filter_sessions] - start_time_to: {start_time_to}")
+    logger.debug(f"[filter_sessions] - available_seat: {available_seat}")
     sessions = read_session_csv()
-    logger.info(f"Fetching sessions by attribute: {field} with value: {value}")
-    
-    if field not in Session.__annotations__:
-        raise HTTPException(
-            status_code=HTTPStatus.BAD_REQUEST,
-            detail="Campo inválido"
-        )
-
-    try:
-        if field in ["id", "movie_id"]:
-            value = int(value)
-        elif field == "start_time":
-            value = datetime.fromisoformat(value)
-    except ValueError as e:
-        raise HTTPException(
-            status_code=HTTPStatus.BAD_REQUEST,
-            detail=f"Valor inválido para o campo {field}: {e}"
-        )
-
-    filtered = []
+    results: List[Session] = []
     for session in sessions:
-        session_value = getattr(session, field)
-        
-        if field == "available_seats":
-            if value in session_value:
-                filtered.append(session)
-        else:
-            if session_value == value:
-                filtered.append(session)
-
-    if not filtered:
-        raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND,
-            detail="Nenhuma sessão encontrada com o atributo especificado"
-        )
-    
-    return filtered
+        if movie_id is not None and session.movie_id != movie_id:
+            continue
+        if room is not None and session.room.lower() != room.lower():
+            continue
+        if start_time_from is not None and session.start_time < start_time_from:
+            continue
+        if start_time_to   is not None and session.start_time > start_time_to:
+            continue
+        if available_seat is not None and available_seat not in session.available_seats:
+            continue
+        results.append(session)
+    return results
 
 #F6 Retornar o Hash SHA256 do Arquivo CSV
 @router.get("/sessions-hash")
 def get_sessions_hash():
-    logger.info("Calculating SHA256 hash of the session CSV file")
+    logger.info("[get_sessions_hash - Calculating SHA256 hash of the session CSV file")
     import hashlib
     sha256_hash = hashlib.sha256()
     with open(SESSION_CSV_FILE, "rb") as f:
@@ -178,7 +166,7 @@ def get_sessions_hash():
 #F8 Converter o csv para xml
 @router.get("/sessions-xml")
 def get_sessions_xml():
-    logger.info("Converting sessions CSV to XML")
+    logger.info("[get_sessions_xml] - Converting sessions CSV to XML")
     import xml.etree.ElementTree as ET
     sessions = read_session_csv()
     root = ET.Element("sessions")
@@ -188,6 +176,6 @@ def get_sessions_xml():
             child = ET.SubElement(session_elem, key)
             child.text = str(value)
     tree = ET.ElementTree(root)
-    xml_file_path = 'xml_files/sessions.xml'
+    xml_file_path = sessions_data.get('xml', {}).get('session', 'xml_files/sessions.xml')
     tree.write(xml_file_path, encoding='utf-8', xml_declaration=True)
     return FileResponse(xml_file_path, media_type="application/xml", filename=os.path.basename(xml_file_path))
